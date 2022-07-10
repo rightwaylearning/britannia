a93f7108f12e034140105b2d80ec0ebd41c7f446
sachin <abc@gmail.com>
2022-07-10 09:17:19 +0530

    this addition program us files

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition{
+   int a;
+   int b;
+
+   void doSum(){
+     int c = a+ b;
+     System.out.println(c);
+   }
+
+}
---
+++ EntryPointOfJVM.java
@@ -0,0 +1,11 @@
+class EntryPointOfJVM{
+
+  public static void main(String[] data){
+
+    Addition add = new Addition();
+    add.a = 10;
+    add.b = 20;
+    add.doSum();
+ }
+
+}
